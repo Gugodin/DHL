17264dd7e1a1a85c29bf815e07a4cf93dc299b79
JavierGutierrez <gugodin@hotmail.com>
2022-02-18 22:57:32 -0600

    Almost finished

--- AlgoGPuntos.py
+++ AlgoGPuntos.py
@@ -1,78 +1,74 @@
-from glob import glob
-import re
-from turtle import goto
+import sys
+from PySide6.QtCore import *
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 from Ind import Ind
-import random  
+import random
 import cruza as repro
+from PySide6.QtWidgets import *
+from view.PrimeraVentana import Ui_MainWindow
 
-#PARAMETROS PARA LO DE LAS CIUDADES
-IntervaloX = [-3,3]
-IntervaloY = [-4,4]
+# from main import ventana
+
+# PARAMETROS PARA LO DE LAS CIUDADES
+IntervaloX = [-3, 3]
+IntervaloY = [-4, 4]
 NoCiudades = 6
 InitialCity = None
 Cities = []
 Population = []
 
-#PARAMETROS PARA EL ALGORITMO GENETICO
+# PARAMETROS PARA EL ALGORITMO GENETICO
 InitialPopulation = 6
-MaxPopulation =10
+MaxPopulation = 10
 ProbMutation = 0.5
 ProbMutationGen = 0.05
-numGeneration = 5
+numGeneration = 20
 Generations = {}
 
 
-
-
-
-def generateCities ():
+def generateCities():
     global InitialCity
 
-    #Generando las ciudades al azar
+    # Generando las ciudades al azar
 
     i = 0
-    while(i < NoCiudades):
+    while (i < NoCiudades):
         currentCity = []
 
-        randX = random.randint(IntervaloX[0],IntervaloX[1])
-        randY = random.randint(IntervaloY[0],IntervaloY[1])
+        randX = random.randint(IntervaloX[0], IntervaloX[1])
+        randY = random.randint(IntervaloY[0], IntervaloY[1])
 
         currentCity.append(randX)
         currentCity.append(randY)
 
-        
         if Cities.count(currentCity) == 0:
             Cities.append(currentCity)
             i += 1
-        
 
-    #Tomando un numero random entre 0 y el total de ciudades para generar la ciudad inicial
+    # Tomando un numero random entre 0 y el total de ciudades para generar la ciudad inicial
 
-    InitialCity = Cities[random.randint(0,len(Cities)-1)]
-
-
-
+    InitialCity = Cities[random.randint(0, len(Cities) - 1)]
 
 
 def generatePopulation():
-
-    #Generamos una lista temporal de las ciudades para poder hacerle modificaciones
+    # Generamos una lista temporal de las ciudades para poder hacerle modificaciones
     citiesToAdd = Cities.copy()
 
-    #Sacamos la ciudad inicial del la lista temporal para no volverla agregarla
+    # Sacamos la ciudad inicial del la lista temporal para no volverla agregarla
     citiesToAdd.pop(citiesToAdd.index(InitialCity))
-     
+
     for i in range(InitialPopulation):
 
         currentCities = citiesToAdd.copy()
 
-        #Colocamos la ruta de cada individuo
-        currentRuta = [InitialCity,InitialCity]
-        
+        # Colocamos la ruta de cada individuo
+        currentRuta = [InitialCity, InitialCity]
+
         # Toma numeros randoms entre los index de currentCities
         for x in range(len(currentCities)):
-            city = currentCities.pop(random.randint(0,len(currentCities)-1))
-            currentRuta.insert(1,city)
+            city = currentCities.pop(random.randint(0, len(currentCities) - 1))
+            currentRuta.insert(1, city)
 
         ind = Ind(currentRuta)
 
@@ -82,11 +78,7 @@ def generatePopulation():
         # print(ind.toString())
 
 
-
-
-
 def mating():
-
     global Population
 
     pTemp = []
@@ -94,21 +86,19 @@ def mating():
     for i in range(len(Population)):
         pTemp.append(Population[i])
 
-    
     parents = []
-    
-    #Generamos parejas de 2 o 1 aleatoriamente
+
+    # Generamos parejas de 2 o 1 aleatoriamente
 
     while len(pTemp) != 0:
 
-        partner1 = pTemp.pop(random.randint(0,len(pTemp)-1))
+        partner1 = pTemp.pop(random.randint(0, len(pTemp) - 1))
 
         if len(pTemp) != 0:
-            partner2 = pTemp.pop(random.randint(0,len(pTemp)-1))
-            parents.append([partner1,partner2])
+            partner2 = pTemp.pop(random.randint(0, len(pTemp) - 1))
+            parents.append([partner1, partner2])
         else:
             parents.append([partner1])
-
 
     children = cruza(parents)
 
@@ -116,48 +106,37 @@ def mating():
         Population.append(child)
 
 
-
-
-
 def cruza(parents):
-
     children = []
-    
+
     for i in range(len(parents)):
 
-
         if len(parents) > 1:
-            
             p1 = parents[i][0].ruta.copy()
             p2 = parents[i][1].ruta.copy()
 
-        
             p1.pop(0)
             p2.pop(0)
-            p1.pop(len(p1)-1)
-            p2.pop(len(p2)-1)     
+            p1.pop(len(p1) - 1)
+            p2.pop(len(p2) - 1)
 
-            ch1,ch2 = repro.pmx(p1,p2)
+            ch1, ch2 = repro.pmx(p1, p2)
 
-
-            ch1.insert(0,InitialCity)
-            ch2.insert(0,InitialCity)
-            ch1.insert(len(ch1),InitialCity)
-            ch2.insert(len(ch2),InitialCity)
+            ch1.insert(0, InitialCity)
+            ch2.insert(0, InitialCity)
+            ch1.insert(len(ch1), InitialCity)
+            ch2.insert(len(ch2), InitialCity)
 
             # print(f'Ciudad inicial {InitialCity}')
-
-            
 
             child1 = Ind(ch1)
             child2 = Ind(ch2)
 
             children.append(child1)
             children.append(child2)
-    
+
     return children
 
-           
 
 def poda():
     global Population
@@ -165,12 +144,12 @@ def poda():
     numEliminations = len(Population) - MaxPopulation
 
     aptitudes = []
-    
+
     for i in range(len(Population)):
         aptitudes.append(Population[i].gasto)
 
     for i in range(numEliminations):
-    
+
         for x in range(len(aptitudes)):
 
             if aptitudes[x] == max(aptitudes):
@@ -178,8 +157,115 @@ def poda():
                 aptitudes.pop(x)
                 break
 
-            
-    
+
+class Ventana(QMainWindow):
+    listaPermisos = []
+
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton.clicked.connect(self.generateMap)
+
+    def generateMap(self):
+        global IntervaloX
+        global IntervaloY
+        global NoCiudades
+
+        xTemp = []
+        yTemp = []
+
+        xTemp.append(int(self.ui.x1.text()))
+        xTemp.append(int(self.ui.x2.text()))
+
+        yTemp.append(int(self.ui.y1.text()))
+        yTemp.append(int(self.ui.y2.text()))
+
+        xTemp = sorted(xTemp)
+        yTemp = sorted(yTemp)
+
+        IntervaloX = xTemp.copy()
+        IntervaloY = yTemp.copy()
+        NoCiudades = int(self.ui.nuCiudades.text())
+
+        generateCities()
+
+        # print(Cities)
+
+        self.generateFirtsMap()
+
+        plt.show()
+
+        generatePopulation()
+        
+        for i in range(numGeneration):
+
+            mating()
+
+            if len(Population) > MaxPopulation:
+                poda()
+
+            Generations[f'gen{i+1}'] = Population.copy()
+
+        self.generateLastMap()
+
+
+    def generateFirtsMap(self):
+
+        figure2 = plt.figure(figsize=(15, 10))
+
+        ax = plt.subplot(1,1,1)
+
+        ax.set_title('Mapa de ciudades generadas')
+
+        ax.plot(IntervaloX[0], IntervaloY[0], marker='o', lw=0, visible=False)
+        ax.plot(IntervaloX[1], IntervaloY[1], marker='o', lw=0, visible=False)
+
+
+        for x in range(len(Cities)):
+
+            ax.plot(Cities[x][0], Cities[x][1], marker='o', lw=0)
+
+        ax.plot(InitialCity[0], InitialCity[1], marker='x', lw=0,color='k')
+
+
+    def generateLastMap(self):
+
+        figure2 = plt.figure(figsize=(15, 10))
+
+        ax = plt.subplot(1, 1, 1)
+
+        ax.set_title('Mapa de ruta')
+
+        aptitudes = []
+
+        for i in range(len(Generations[f'gen{numGeneration}'])):
+            aptitudes.append(Population[i].gasto)
+
+
+        # print(f'Aptitudes: {aptitudes}')
+        # print(f'Minimo: {aptitudes.index(min(aptitudes))}')
+
+        ind = Generations[f'gen{numGeneration}'][aptitudes.index(min(aptitudes))]
+
+        print(ind.ruta)
+        # print(InitialCity)
+        xs = []
+        ys = []
+
+        for i in range(len(ind.ruta)):
+            xs.append(ind.ruta[i][0])
+            ys.append(ind.ruta[i][1])
+
+
+        ax.plot(xs, ys, marker='o',ls='dotted')
+        ax.plot(InitialCity[0], InitialCity[1], marker='x', lw=0,color='k')
+
+
+
+        plt.show()
+
 
 
 
@@ -190,23 +276,24 @@ def poda():
 if __name__ == '__main__':
 
     for i in range(numGeneration):
-        Generations.update({f'gen{i+1}':[]})
-    
-    generateCities()
-     
-    generatePopulation()
+        Generations.update({f'gen{i + 1}': []})
 
-    for i in range(numGeneration):
+    # generateCities()
 
-        mating()
+    # generatePopulation()
 
-        if len(Population) > MaxPopulation:
-            poda()
+    # for i in range(numGeneration):
 
-        Generations[f'gen{i+1}'] = Population.copy()
-        
+    #     mating()
 
-    print(Generations)
+    #     if len(Population) > MaxPopulation:
+    #         poda()
 
+    #     Generations[f'gen{i+1}'] = Population.copy()
 
-    
+    # # print(Generations)
+
+    app = QApplication(sys.argv)
+    window = Ventana()
+    window.show()
+    sys.exit(app.exec())
